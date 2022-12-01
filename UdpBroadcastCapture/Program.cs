781353461d0152b3f4e234db9d132b404339b83a
Maria Nijar <r0830560@student.vives.be>
2022-12-01 10:41:03 +0100

    Refactored and commented the code

--- UdpBroadcastCapture/Program.cs
+++ UdpBroadcastCapture/Program.cs
@@ -7,14 +7,10 @@ namespace UdpBroadcastCapture
 {
     class Program
     {
-        // https://msdn.microsoft.com/en-us/library/tst0kwb1(v=vs.110).aspx
         // IMPORTANT Windows firewall must be open on UDP port 7000
-        // https://www.windowscentral.com/how-open-port-windows-firewall
-        // Use the network MGV-xxx to capture from local IoT devices (fake or real)
+        // Use the network MGV-xxx to capture from local IoT devices 
         private const int Port = 7000 ;
-        //private static readonly IPAddress IpAddress = IPAddress.Parse("192.168.5.137"); 
-        // Listen for activity on all network interfaces
-        // https://msdn.microsoft.com/en-us/library/system.net.ipaddress.ipv6any.aspx
+
         static void Main()
         {
             IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, Port);
@@ -29,22 +25,11 @@ namespace UdpBroadcastCapture
                     string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
                     Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
                         remoteEndPoint.Address, remoteEndPoint.Port, message);
-                    //Parse(message);
+                 
                 }
             }
         }
 
-        // To parse data from the IoT devices (depends on the protocol)
-        //private static void Parse(string response)
-        //{
-        //    string[] parts = response.Split(' ');
-        //    foreach (string part in parts)
-        //    {
-        //        Console.WriteLine(part);
-        //    }
-        //    string temperatureLine = parts[6];
-        //    string temperatureStr = temperatureLine.Substring(temperatureLine.IndexOf(": ") + 2);
-        //    Console.WriteLine(temperatureStr);
-        //}
+        
     }
 }
